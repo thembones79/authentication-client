5195b5737ef202b314226602aea5ced31e12e8d6
thembones79 <michal.szumnarski@gmail.com>
2019-12-30 23:44:49 +0100

    auth chcecker

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -6,7 +6,9 @@ import * as actions from "../../actions";
 
 class Signup extends React.Component {
   onSubmit = formProps => {
-    this.props.signup(formProps);
+    this.props.signup(formProps, () => {
+      this.props.history.push("/feature");
+    });
   };
   render() {
     const { handleSubmit } = this.props;
@@ -30,13 +32,18 @@ class Signup extends React.Component {
             autoComplete="none"
           />
         </fieldset>
+        <div>{this.props.errorMessage}</div>
         <button>Sign Up!</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
 export default compose(
-  connect(null, actions),
+  connect(mapStateToProps, actions),
   reduxForm({ form: "signup" })
 )(Signup);
